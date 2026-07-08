2908aa3709469994e164aee42524510c42cd59df
SP <narrasaisree@gmail.com>
2026-07-08 19:15:47 +0530

    Create variables.tf for production environment
    Added various Terraform variables for production environment configuration.

---
+++ infra/envs/prod/variables.tf
@@ -0,0 +1,97 @@
+variable "project_name" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "vpc_cidr" {
+  type = string
+}
+
+variable "public_subnet_1_cidr" {
+  type = string
+}
+
+variable "public_subnet_2_cidr" {
+  type = string
+}
+
+variable "private_subnet_1_cidr" {
+  type = string
+}
+
+variable "private_subnet_2_cidr" {
+  type = string
+}
+
+variable "database_subnet_1_cidr" {
+  type = string
+}
+
+variable "database_subnet_2_cidr" {
+  type = string
+}
+
+variable "availability_zone_1" {
+  type = string
+}
+
+variable "availability_zone_2" {
+  type = string
+}
+
+variable "db_name" {
+  type = string
+}
+
+variable "db_username" {
+  type = string
+}
+
+variable "db_password" {
+  type      = string
+  sensitive = true
+}
+
+variable "instance_class" {
+  type = string
+}
+
+variable "allocated_storage" {
+  type = number
+}
+
+variable "backup_retention_period" {
+  type = number
+}
+
+variable "deletion_protection" {
+  type = bool
+}
+
+variable "container_image" {
+  type = string
+}
+
+variable "container_port" {
+  type = number
+}
+
+variable "cpu" {
+  type = number
+}
+
+variable "memory" {
+  type = number
+}
+
+variable "desired_count" {
+  type = number
+}
+
+variable "aws_region" {
+  type    = string
+  default = "us-east-1"
+}
