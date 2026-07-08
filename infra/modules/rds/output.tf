c73a9418f10c9ee743e80a8d41ec0bc3476c096a
SP <narrasaisree@gmail.com>
2026-07-08 19:26:21 +0530

    Add outputs for RDS instance details

---
+++ infra/modules/rds/output.tf
@@ -0,0 +1,15 @@
+output "db_endpoint" {
+  value = aws_db_instance.postgres.address
+}
+
+output "db_port" {
+  value = aws_db_instance.postgres.port
+}
+
+output "db_name" {
+  value = aws_db_instance.postgres.db_name
+}
+
+output "db_identifier" {
+  value = aws_db_instance.postgres.id
+}
